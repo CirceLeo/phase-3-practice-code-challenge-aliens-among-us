1da5ba5e4c054332033aeba6a028b62993edb755
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-13 10:59:00 -0400

    most frequent is poorly written but functional

--- app/models/alien.rb
+++ app/models/alien.rb
@@ -13,11 +13,22 @@ class Alien < ActiveRecord::Base
     end
 
     def self.most_frequent_visitor
-        Visitation.group(:alien_id).order('DESC')
+        #Visitation.order('COUNT(*) DESC').select('alien_id')
+        most_frequent_id = 1
+        most_visits = 0
+        Alien.all.each do |alien|
+            num_visits = Visitation.where(alien_id: alien.id).length
+            if num_visits > most_visits 
+                most_visits = num_visits
+                most_frequent_id = alien.id
+            end
+        end
+        Alien.find(most_frequent_id)
     end
 
     def self.average_light_years_to_home_planet
-        total_ly = Alien.all.sum{|alien| alien.light_years_to_home_planet}
+        #total_ly = Alien.all.sum{|alien| alien.light_years_to_home_planet}
+        total_ly = Alien.sum(:light_years_to_home_planet)
         total_ly / Alien.count
     end
 
